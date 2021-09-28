75f9646c4297f8077988c6c0ab247212872b5fda
Wadim <lavrentiev.wadim150902@gmail.com>
2021-09-29 01:45:39 +0300

    not working variant

---
+++ src/ru/mirea/task7/opt2/MovableCircle.java
@@ -0,0 +1,39 @@
+package ru.mirea.task7.opt2;
+
+public class MovableCircle extends MovablePoint implements Movable{
+    private int radius;
+    private MovablePoint center;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius){
+        super(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "MovableCircle{" +
+                "radius=" + this.radius +
+                ", center=" + this.center +
+                '}';
+    }
+
+    @Override
+    public void moveUp() {
+        this.center.moveUp();
+    }
+
+    @Override
+    public void moveDown() {
+        this.center.moveDown();
+    }
+
+    @Override
+    public void moveLeft() {
+        this.center.moveLeft();
+    }
+
+    @Override
+    public void moveRight() {
+        this.center.moveRight();
+    }
+}
---
+++ src/ru/mirea/task7/opt2/MovableRectangle.java
@@ -0,0 +1,12 @@
+package ru.mirea.task7.opt2;
+
+public class MovableRectangle extends MovablePoint implements Movable{
+    private MovablePoint topLeft;
+    private MovablePoint bottomRight;
+
+    public MovableRectangle(int x1, int y1, int x2, int y2,
+                            int xSpeed, int ySpeed){
+        super();
+
+    }
+}
